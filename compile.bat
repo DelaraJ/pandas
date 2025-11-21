92d4e422b204db32d75621e68f24a8744837118a
DelaraJ <delara.jalali.382@gmail.com>
2025-11-21 07:47:11 +0330

    fix compile file

--- compile.bat
+++ compile.bat
@@ -1,8 +1,3 @@
-#!/bin/bash
-set -e
-
-echo "Building pandas docker image..."
+@echo off
 docker build -t pandas-custom-runner .\tooling\debug\Dockerfile.pandas-debug
-
-echo "Running python compileall..."
 docker run --rm pandas-custom-runner python -m compileall .
