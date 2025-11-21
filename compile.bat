1ff1cb16390fe94b369ea31cfe01a3a3844576a5
DelaraJ <delara.jalali.382@gmail.com>
2025-11-21 08:03:47 +0330

    check simpler compile file

--- compile.bat
+++ compile.bat
@@ -1,3 +1,4 @@
 @echo off
-docker build -t pandas-custom-runner .\tooling\debug\Dockerfile.pandas-debug
-docker run --rm pandas-custom-runner python -m compileall .
+echo Running compileall...
+python -m compileall .
+exit /b %errorlevel%
